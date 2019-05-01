47fb0f28e13752fad7aea3f6624e771dbd6cd5ac
Wamiq <wamiqurehman093@gmail.com>
2019-05-01 23:44:34 +0500

    added walking animation

--- kayzee.py
+++ kayzee.py
@@ -1,4 +1,5 @@
 import arcade
+from player import Player, get_distance_between_sprites
 
 SCREEN_WIDTH = 1000
 SCREEN_HEIGHT = 650
@@ -34,7 +35,7 @@ class MyGame(arcade.Window):
         self.dont_touch_list = None
         self.player_list = None
 
-        self.player_sprite = None
+        self.player = None
         self.physics_engine = None
 
         self.view_bottom = 0
@@ -63,10 +64,35 @@ class MyGame(arcade.Window):
         self.foreground_list = arcade.SpriteList()
         self.background_list = arcade.SpriteList()
 
-        self.player_sprite = arcade.Sprite("images/player.png", CHARACTER_SCALING)
-        self.player_sprite.center_x = PLAYER_START_X
-        self.player_sprite.center_y = PLAYER_START_Y
-        self.player_list.append(self.player_sprite)
+        # self.player_sprite = arcade.Sprite("images/player.png", CHARACTER_SCALING)
+        # self.player_sprite.center_x = PLAYER_START_X
+        # self.player_sprite.center_y = PLAYER_START_Y
+        # self.player_list.append(self.player_sprite)
+
+        self.player = Player()
+
+        self.player.stand_right_textures = []
+        self.player.stand_left_textures = []
+        self.player.walk_right_textures = []
+        self.player.walk_left_textures = []
+
+        self.player.stand_right_textures.append(arcade.load_texture("images/player_3/stand/0.png",
+                                                                    scale=CHARACTER_SCALING))
+        self.player.stand_left_textures.append(arcade.load_texture("images/player_3/stand/0.png",
+                                                                   scale=CHARACTER_SCALING, mirrored=True))
+        for i in range(8):
+            self.player.walk_right_textures.append(arcade.load_texture("images/player_3/walk/"+str(i)+".png",
+                                                                       scale=CHARACTER_SCALING))
+        for i in range(8):
+            self.player.walk_left_textures.append(arcade.load_texture("images/player_3/walk/"+str(i)+".png",
+                                                                      scale=CHARACTER_SCALING, mirrored=True))
+
+        self.player.texture_change_distance = 30
+
+        self.player.center_x = SCREEN_WIDTH // 2
+        self.player.center_y = SCREEN_HEIGHT // 2
+        self.player.scale = 0.2
+        self.player_list.append(self.player)
 
         platforms_layer_name = "Platforms"
         coins_layer_name = "Coins"
@@ -91,7 +117,7 @@ class MyGame(arcade.Window):
         if my_map.backgroundcolor:
             arcade.set_background_color(my_map.backgroundcolor)
 
-        self.physics_engine = arcade.PhysicsEnginePlatformer(self.player_sprite,
+        self.physics_engine = arcade.PhysicsEnginePlatformer(self.player,
                                                              self.wall_list,
                                                              GRAVITY)
 
@@ -102,7 +128,10 @@ class MyGame(arcade.Window):
         self.wall_list.draw()
         self.coin_list.draw()
         self.dont_touch_list.draw()
-        self.player_list.draw()
+        try:
+            self.player_list.draw()
+        except:
+            pass
         self.foreground_list.draw()
 
         score_text = f"Score: {self.score}"
@@ -113,23 +142,24 @@ class MyGame(arcade.Window):
     def on_key_press(self, symbol: int, modifiers: int):
         if symbol == arcade.key.UP or symbol == arcade.key.W:
             if self.physics_engine.can_jump():
-                self.player_sprite.change_y = JUMP_SPEED
+                self.player.change_y = JUMP_SPEED
                 arcade.play_sound(self.jump_sound)
         elif symbol == arcade.key.LEFT or symbol == arcade.key.A:
-            self.player_sprite.change_x = -MOVEMENT_SPEED
+            self.player.change_x = -MOVEMENT_SPEED
         elif symbol == arcade.key.RIGHT or symbol == arcade.key.D:
-            self.player_sprite.change_x = MOVEMENT_SPEED
+            self.player.change_x = MOVEMENT_SPEED
 
     def on_key_release(self, symbol: int, modifiers: int):
         if symbol == arcade.key.LEFT or symbol == arcade.key.A:
-            self.player_sprite.change_x = 0
+            self.player.change_x = 0
         elif symbol == arcade.key.RIGHT or symbol == arcade.key.D:
-            self.player_sprite.change_x = 0
+            self.player.change_x = 0
 
     def update(self, delta_time: float):
         self.physics_engine.update()
+        self.player_list.update_animation()
 
-        coin_hitlist = arcade.check_for_collision_with_list(self.player_sprite,
+        coin_hitlist = arcade.check_for_collision_with_list(self.player,
                                                             self.coin_list)
         for coin in coin_hitlist:
             coin.remove_from_sprite_lists()
@@ -138,9 +168,9 @@ class MyGame(arcade.Window):
 
         changed_viewport = False
 
-        if self.player_sprite.center_y < 100:
-            self.player_sprite.center_x = PLAYER_START_X
-            self.player_sprite.center_Y = PLAYER_START_Y
+        if self.player.center_y < 100:
+            self.player.center_x = PLAYER_START_X
+            self.player.center_Y = PLAYER_START_Y
 
             self.view_left = 0
             self.view_bottom = 0
@@ -148,9 +178,9 @@ class MyGame(arcade.Window):
 
             arcade.play_sound(self.game_over)
 
-        if arcade.check_for_collision_with_list(self.player_sprite, self.dont_touch_list):
-            self.player_sprite.center_x = PLAYER_START_X
-            self.player_sprite.center_y = PLAYER_START_Y
+        if arcade.check_for_collision_with_list(self.player, self.dont_touch_list):
+            self.player.center_x = PLAYER_START_X
+            self.player.center_y = PLAYER_START_Y
 
             # Set the camera to the start
             self.view_left = 0
@@ -158,7 +188,7 @@ class MyGame(arcade.Window):
             changed_viewport = True
             arcade.play_sound(self.game_over)
 
-        if self.player_sprite.center_x >= self.end_of_map:
+        if self.player.center_x >= self.end_of_map:
             print("Advance ****")
             self.level += 1
             self.setup(self.level)
@@ -168,23 +198,23 @@ class MyGame(arcade.Window):
             changed_viewport = True
 
         left_boundary = self.view_left + LEFT_VIEWPORT_MARGIN
-        if self.player_sprite.left < left_boundary:
-            self.view_left -= left_boundary - self.player_sprite.left
+        if self.player.left < left_boundary:
+            self.view_left -= left_boundary - self.player.left
             changed_viewport = True
 
         right_boundary = self.view_left + SCREEN_WIDTH - RIGHT_VIEWPORT_MARGIN
-        if self.player_sprite.right > right_boundary:
-            self.view_left += self.player_sprite.right - right_boundary
+        if self.player.right > right_boundary:
+            self.view_left += self.player.right - right_boundary
             changed_viewport = True
 
         top_boundary = self.view_bottom + SCREEN_HEIGHT - TOP_VIEWPORT_MARGIN
-        if self.player_sprite.top > top_boundary:
-            self.view_bottom += self.player_sprite.top - top_boundary
+        if self.player.top > top_boundary:
+            self.view_bottom += self.player.top - top_boundary
             changed_viewport = True
 
         bottom_boundary = self.view_bottom + BOTTOM_VIEWPORT_MARGIN
-        if self.player_sprite.bottom < bottom_boundary:
-            self.view_bottom -= bottom_boundary - self.player_sprite.bottom
+        if self.player.bottom < bottom_boundary:
+            self.view_bottom -= bottom_boundary - self.player.bottom
             changed_viewport = True
 
         if changed_viewport:
